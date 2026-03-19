d3d4af2db43585b8264f699d57685b0aa62b9397
Mohammad Umar <umamohd8@gmail.com>
2026-03-19 23:50:55 +0530

    Navigation back button added for movie details pages

--- src/Components/MovieDetailsInfo.jsx
+++ src/Components/MovieDetailsInfo.jsx
@@ -3,8 +3,11 @@ import { useState, useEffect } from "react";
 import { fetchMovieDetails, fetchMovieVideos, fetchMovieCredits,fetchWatchProviders} from "../services/api";
 import MovieDetailsCard from "./MovieDetailsCard";
 import SkeletonCardMovieDetails from "./SkeletonCardMovieDetails";
+import { useNavigate } from "react-router-dom";
 
 export default function MovieDetailsInfo() {
+
+  const navigate = useNavigate();
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
   const [videos, setVideos] = useState([]);
@@ -35,6 +38,16 @@ export default function MovieDetailsInfo() {
     );
   return (
     <div>
+      {/* Back Button */}
+    <div className="px-4 pt-4 -mb-10 relative z-10">
+      <button
+        onClick={() => navigate(-1)}
+        className="flex items-center gap-2 text-white text-sm px-4 py-2 rounded-full bg-zinc-900/80 backdrop-blur-md border border-zinc-700 hover:bg-zinc-800 transition shadow"
+      >
+        ← Back
+      </button>
+    </div>
+
       <MovieDetailsCard
         movie={movie}
         videos={videos}
